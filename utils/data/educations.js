2cb1735c813eba3c030b0be1724bdec14bd4d8fc
harsh6754 <agarwalharsh6583@gmail.com>
2024-03-25 21:52:59 +0530

    Education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,19 +1,19 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
+    title: "Bachelor's Degree",
     duration: "2021 - Present",
     institution: "Parul University, Vadodara",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
+    title: "Senior Secondary ",
     duration: "2019 - 2021",
     institution: "Career Academy Senior Secondary School",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
+    title: "Secondary School",
     duration: "2009 - 2019",
     institution: "Modern Wisdom Senior Secondary School",
   }
