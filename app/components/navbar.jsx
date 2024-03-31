4a3a1085c6d7a1bb389cce925dd79cca837a86d6
harsh6754 <agarwalharsh6583@gmail.com>
2024-03-31 09:16:24 +0530

    Achivement Page

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -23,6 +23,9 @@ function Navbar() {
           <li>
             <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></a>
           </li>
+          {/* <li>
+            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#Achivement"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ACHIVEMENT</div></a>
+          </li> */}
           <li>
             <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></a>
           </li>
