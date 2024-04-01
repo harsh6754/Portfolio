6ba984cef88e4868988b3866eab67ed90d6fd921
harsh6754 <agarwalharsh6583@gmail.com>
2024-04-01 15:42:18 +0530

    Achivement Section Added

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import Achivement from "./components/homepage/achivement";
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -30,6 +31,7 @@ export default async function Home() {
       <HeroSection />
       <AboutSection />
       <Experience />
+      <Achivement/>
       <Skills />
       <Projects />
       <Education />
