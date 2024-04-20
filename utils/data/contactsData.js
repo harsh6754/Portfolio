9f2050f927932d424e629743714752fc794c779a
harsh6754 <agarwalharsh6583@gmail.com>
2024-04-20 11:09:18 +0530

    Contact Page

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,9 +3,9 @@ export const contactsData = {
     phone: '+91-9636504390',
     address: '107 Shrimal Nagar, Bhinmal, Jalor - 343029 , Rajasthan, India',
     github: 'https://github.com/harsh6754',
-    facebook: 'https://www.facebook.com//',
+    facebook: 'https://www.facebook.com/profile.php?id=100045533649663',
     linkedIn: 'https://www.linkedin.com/in/harsh2810/',
-    twitter: 'https://twitter.com/',
-    stackOverflow: 'https://stackoverflow.com/users/',
+    twitter: 'https://twitter.com/HarshAg62381240',
+    stackOverflow: 'https://stackoverflow.com/users/19534565/harsh-agrawal',
     devUsername: ""
 }
