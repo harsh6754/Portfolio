c8a903321462b836a462fae85d72759eeecd69de
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-13 13:42:41 +0530

    Add Certficate Page

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
-          {children}
+            {children}
         </main>
         <Footer />
       </body>
