39441c0ab17a37613abfee6ad7512156ddb2534c
harsh6754 <agarwalharsh6583@gmail.com>
2024-12-31 15:44:58 +0530

    Update Portfolio

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,26 @@
 export const experiences = [
   {
-    id: 1,
+    id:1,
+    title:"Software Engineer-I",
+    company:"Casepoint llc",
+    duration:"(Jan 2025 - Present)"
+  },
+  {
+    id: 2,
     title: 'Full Stack Developer ',
     company: "CodSoft Pvt Ltd",
     duration: "(April 2023 - June 2023)"
   },
   {
-    id: 2,
+    id: 3,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
     duration: "(Jun 2021 - Present)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Digifrills It Solution",
     company: "Code and build something in everyday.",
-    duration: "(March 2024 - Present)"
+    duration: "(March 2024 - May 2024)"
   }
 ]
